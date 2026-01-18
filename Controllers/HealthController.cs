7642ae21c75300b143efdc131e1e28fa57418187
Alexandr <sanoktabx@gmail.com>
2026-01-19 00:44:16 +0500

    Initial ASP.net core web api

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlowREST.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new {status = "ok"});
+    }
+}
